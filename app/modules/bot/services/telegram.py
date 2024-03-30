107302ec1ad08dc7a0de47249e04ea2a38e416c5
NickiHell <nickihell@ya.ru>
2024-03-31 09:38:21 +1000

    refactor: refactor

--- app/modules/bot/services/telegram.py
+++ app/modules/bot/services/telegram.py
@@ -10,30 +10,30 @@ from loguru import logger
 from app.di import nn_container
 from app.modules.neural_networks.services.dialogpt import DialogGPTNN
 
-API_TOKEN = os.getenv("BOT_TOKEN", None)
-BOT_USERNAME = os.getenv("BOT_USERNAME", None)
+API_TOKEN = os.getenv('BOT_TOKEN', None)
+BOT_USERNAME = os.getenv('BOT_USERNAME', None)
 BOT = Bot(API_TOKEN, parse_mode=ParseMode.HTML)
 DP = Dispatcher()
 
 
 @DP.message(CommandStart())
 async def start(message: types.Message) -> NoReturn:
-    await message.reply("Твой отец тебя бросил...")
+    await message.reply('Твой отец тебя бросил...')
 
 
 @DP.message()
 async def handle_message(message: types.Message) -> NoReturn:
-    logger.info(f"Message: {message.text}")
+    logger.info(f'Message: {message.text}')
     async with nn_container.context() as ctx:
-        service = await ctx.resolve(DialogGPTNN)
+        service  = await ctx.resolve(DialogGPTNN)
 
-        if message.text is not None and f"@{BOT_USERNAME}" in message.text:
+        if message.text is not None and f'@{BOT_USERNAME}' in message.text:
             await get_answer(message, service)
 
         if message.reply_to_message is not None and message.reply_to_message.from_user.is_bot is True:
             await get_answer(message, service)
 
-        if message.chat.type == "private":
+        if message.chat.type == 'private':
             await get_answer(message, service)
 
 
@@ -42,6 +42,6 @@ async def get_answer(
     message: types.Message,
     service: Annotated[DialogGPTNN, Inject] = None,
 ) -> None:
-    uid = await service.put_request(message.text.replace(f"@{BOT_USERNAME} ", ""))
+    uid = await service.put_request(message.text.replace(f'@{BOT_USERNAME} ', ''))
     result = await service.get_response(uid)
     await message.reply(result)
